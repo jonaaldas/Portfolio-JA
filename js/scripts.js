96f06cd61b0255d3ce0601febe88709160c74cd4
Jonathan Aldas <jonaaldas@gmail.com>
2022-08-25 17:57:14 -0500

    added my YT videos

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
 //
 // Scripts
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
+    // get data from youtube API
+    fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCy_IrkLSYfaSQL3CNCx9jbA&maxResults=5&order=date&key=AIzaSyBJDm0n7WNmPS6E3132ZHiV5KdxuVpEnDo')
+        .then(res => res.json())
+        .then(data => {
+            const ytData = data.items
+            let ytContainer = document.querySelector('.yt-container')
+            for (let i of ytData) {
+                ytContainer.innerHTML += `
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <a href='https://www.youtube.com/watch?v=${i.id.videoId}'>
+                        <div class="portfolio-item mx-auto" >
+                            <img class="img-fluid" src='${i.snippet.thumbnails.high.url}' alt="..." />
+                            <h3 class='text-secondary'>${i.snippet.title}<h3/>
+                        </div>
+                    </a>
+                </div>              
+                `
+            }
+        })
 
     // Navbar shrink function
     var navbarShrink = function () {
